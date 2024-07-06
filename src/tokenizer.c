373d5623abd44c8f9706182e9fb1bceff8d48ad6
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-07-06 17:46:03 +0900

    take care of comment out (#)

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -177,6 +177,8 @@ t_token *tokenize(char *line, int *status)
 		line = pass_space(line); // スペースを飛ばす
 		if (!*line)
 			break ;
+		if (*line == '#') // コメント行は無視
+			break ;
 		type = check_type(line); // トークンの種類をチェック
 		token_len = add_token(&token, line, type); // トークンを取得
 		if (!token_len)
