77324bc6e7c80dc236b7eacba887428409765aa8
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-07-06 21:05:21 +0900

    動かないよ

--- src/expansion.c
+++ src/expansion.c
@@ -36,7 +36,7 @@ char	*get_variable_key(char *str)
 	i++;
 	while (str[i] && is_alnum_under(str[i]))
 		i++;
-	if (str[i] == ' ')
+	if (str[i] && str[i] == ' ' || str[i] == '\0' || str[i] == '\"')
 		key = ft_substr(str, 1, i - 1);
 	return (NULL);
 }
@@ -61,19 +61,23 @@ void	remove_double_quote(char *str)
 				if (str[i] == '$')
 				{
 					key = get_variable_key(&str[i]);
+					printf("key = %s\n", key);
 					if (key)
 					{
 						value = get_variable_value(key);
+						printf("value = %s\n", value);
+						free(key);
 						if (value)
 						{
+							// realloc
+							str = realloc(str, ft_strlen(str) + ft_strlen(value) + 1);
 							k = 0;
-							while (value[j])
+							while (value[k])
 								str[j++] = value[k++];
 							free(value);
+							i += ft_strlen(key + 1);
 						}
-						free(key);
 					}
-					i += ft_strlen(key + 1);
 				}
 				else
 					str[j++] = str[i++];
