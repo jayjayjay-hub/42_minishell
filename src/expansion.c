66eca9e4c45b1ecf80118edb195be93b95abd545
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-07-10 13:54:44 +0900

    変数完璧だと思う

--- src/expansion.c
+++ src/expansion.c
@@ -24,66 +24,40 @@ void	remove_quote(char *str)
 	str[j] = '\0';
 }
 
-// char	*expansion_variable(char *str)
-// {
-// 	int i;
-// 	int j;
-// 	char *key;
-// 	char *value;
-// 	char quote;
-// 	char *tmp;
-// 	char *tmp2;
+void	variable_loop(char **str, char **tmp, int *i, int *j)
+{
+	char *key;
+	char *value;
+	char quote;
+	char *tmp2;
 
-// 	i = 0;
-// 	j = 0;
-// 	tmp = calloc(1, sizeof(char) * (ft_strlen(str) + 1));
-// 	while (str[i])
-// 	{
-// 		if (is_quote(str[i])) // qoteはそのままコピー
-// 		{
-// 			quote = str[i];
-// 			tmp[j++] = str[i++];
-// 			while (str[i] && str[i] != quote)
-// 				tmp[j++] = str[i++];
-// 			tmp[j++] = str[i++];
-// 		}
-// 		else if (str[i] == '$')
-// 		{
-// 			if (str[i + 1])
-// 			{
-// 				key = get_variable_key(str + i + 1);
-// 				value = get_variable_value(key);
-// 				if (value)
-// 				{
-// 					tmp = ft_strjoin(tmp, value);
-// 					i += ft_strlen(key) + 1;
-// 					j += ft_strlen(value);
-// 					free(key);
-// 					free(value);
-// 				}
-// 				else
-// 					tmp[j++] = str[i++];
-// 			}
-// 			else
-// 				tmp[j++] = str[i++];
-// 		}
-// 		else
-// 			tmp[j++] = str[i++];
-// 	}
-// 	tmp[j] = '\0';
-// 	return (tmp);
-// }
-
+	if ((*str)[*i + 1])
+	{
+		key = get_variable_key((*str) + *i + 1);
+		value = get_variable_value(key);
+		if (value)
+		{
+			tmp2 = ft_strjoin(*tmp, value);
+			free(*tmp);
+			*tmp = tmp2;
+			*i += ft_strlen(key) + 1;
+			*j += ft_strlen(value);
+			free(key);
+			free(value);
+		}
+		else
+			(*tmp)[*j++] = (*str)[*i++];
+	}
+	else
+		(*tmp)[*j++] = (*str)[*i++];
+}
 
 void	expansion_variable(char **str)
 {
 	int i;
 	int j;
-	char *key;
-	char *value;
 	char quote;
 	char *tmp;
-	char *tmp2;
 
 	i = 0;
 	j = 0;
@@ -95,31 +69,16 @@ void	expansion_variable(char **str)
 			quote = (*str)[i];
 			tmp[j++] = (*str)[i++];
 			while ((*str)[i] && (*str)[i] != quote)
-				tmp[j++] = (*str)[i++];
-			tmp[j++] = (*str)[i++];
-		}
-		else if ((*str)[i] == '$')
-		{
-			if ((*str)[i + 1])
 			{
-				key = get_variable_key((*str) + i + 1);
-				value = get_variable_value(key);
-				if (value)
-				{
-					tmp2 = ft_strjoin(tmp, value);
-					free(tmp);
-					tmp = tmp2;
-					i += ft_strlen(key) + 1;
-					j += ft_strlen(value);
-					free(key);
-					free(value);
-				}
+				if ((*str)[i] == '$')
+					variable_loop(str, &tmp, &i, &j);
 				else
 					tmp[j++] = (*str)[i++];
 			}
-			else
-				tmp[j++] = (*str)[i++];
+			tmp[j++] = (*str)[i++];
 		}
+		else if ((*str)[i] == '$')
+			variable_loop(str, &tmp, &i, &j);
 		else
 			tmp[j++] = (*str)[i++];
 	}
@@ -127,7 +86,6 @@ void	expansion_variable(char **str)
 	free(*str);
 	*str = tmp;
 }
-
 
 void	expansion(t_token * token)
 {
