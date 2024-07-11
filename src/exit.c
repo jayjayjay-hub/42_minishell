7086252c21983eba806d0a0c773f9e36868b7e90
Koshiro Nakamura <kosnakam@c3r1s2.42tokyo.jp>
2024-07-11 14:34:28 +0900

    exit 3 OK

--- src/exit.c
+++ src/exit.c
@@ -1,26 +1,44 @@
 
 #include "minishell.h"
 
+bool	ft_str_isdigit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 bool	exit_check(t_token *token)
 {
 	if (!token)
 		return (false);
 	if (!(!ft_strncmp(token->str, "exit", 4) && ft_strlen(token->str) == 4))
-	{
-		// printf("exit_check: %s\n", token->str);
 		return (false);
-	}
 	if (token_list_size(token) > 2)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
 		g_status = 1;
 		free_token(token);
 		token = NULL;
-		return (false);
+		return (true);
 	}
 	else
 	{
 		ft_putendl_fd("exit", 2);
+		if (!token->next)
+			exit(WEXITSTATUS(g_status));
+		if (!ft_str_isdigit(token->next->str))
+		{
+			ft_putendl_fd("minishell: exit: h: numeric argument required", 2);
+			g_status = 258 * 2;
+		}
 		exit(WEXITSTATUS(g_status));
 	}
 }
