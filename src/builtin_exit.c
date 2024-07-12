838bd55deb7c2400b58045bdb17ee77e048c7af0
Koshiro Nakamura <kosnakam@c3r1s2.42tokyo.jp>
2024-07-12 20:04:28 +0900

    exit完璧?

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -8,12 +8,14 @@ bool	ft_str_isdigit(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (!ft_isdigit(str[i]))
+		if (!ft_isdigit(str[i]) && str[i] != '-' && str[i] != '+')
 			return (false);
 		i++;
 	}
 	return (true);
 }
+
+// bool	
 
 bool	builtin_exit(t_token *token)
 {
@@ -28,16 +30,16 @@ bool	builtin_exit(t_token *token)
 	{
 		ft_putendl_fd("exit", 2);
 		if (!token->next)
-			exit(WEXITSTATUS(g_status));
+			exit(g_status);
 		else if (!ft_str_isdigit(token->next->str))
 		{
 			ft_putstr_fd("minishell: exit: ", 2);
 			ft_putstr_fd(token->next->str, 2);
 			ft_putendl_fd(": numeric argument required", 2);
-			g_status = 256 * 2;
+			g_status = 2;
 		}
 		else
-			g_status = 256 * ft_atoi(token->next->str);
-		exit(WEXITSTATUS(g_status));
+			g_status = (size_t)ft_atoi(token->next->str);
+		exit(g_status);
 	}
 }
