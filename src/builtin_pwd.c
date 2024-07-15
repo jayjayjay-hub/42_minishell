3038d4714a22a9fdc8234d0549f91989e29fe7e5
Jay Takahashi <qsakura14555@gmail.com>
2024-07-15 12:54:59 +0900

    不要なコメント削除

--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -9,7 +9,7 @@ bool	builtin_pwd(t_token *token)
 
 	// debug
 	// printf("builtin_pwd\n");
-	// todo コメントアウトしてるif文いらないと思うんだけど確認よろしく
+	// 完了 コメントアウトしてるif文いらないと思うんだけど確認よろしく
 	// 確かにそうだわいらないわ
 	buf = getcwd(NULL, 0);
 	if (!buf)
