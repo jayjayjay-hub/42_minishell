2b22cc7fa45382e5e9ec58d409c9db3fd9d22bda
Koshiro Nakamura <kosnakam@c4r1s4.42tokyo.jp>
2024-07-19 13:48:40 +0900

    echo -n

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -2,18 +2,36 @@
 
 // builtin_echo.c
 
+bool	check_option(char *str)
+{
+	int	i;
+
+	i = 1;
+	if (str[0] != '-')
+		return (false);
+	else
+	{
+		while (str[i] && str[i] == 'n')
+			i++;
+		if (str[i])
+			return (false);
+	}
+	return (true);
+}
+
 bool	builtin_echo(t_token *token)
 {
 	bool	newline;
 
 	newline = true;
-	if (token->next && ft_strlen(token->next->str) == 2
-		&& !ft_strncmp(token->next->str, "-n", 3))
+	if (token->next && check_option(token->next->str))
 	{
 		newline = false;
 		token = token->next;
 	}
 	token = token->next;
+	while (token && check_option(token->str))
+		token = token->next;
 	while (token && token->type == WORD)
 	{
 		write(1, token->str, ft_strlen(token->str));
