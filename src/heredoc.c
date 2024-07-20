5307152051d31c9cf5d421d95bf3edc81d36dfad
Koshiro Nakamura <kosnakam@c3r1s2.42tokyo.jp>
2024-07-20 16:51:00 +0900

    heredoc

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,38 +6,79 @@
 /*   By: kosnakam <kosnakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 15:27:27 by kosnakam          #+#    #+#             */
-/*   Updated: 2024/07/17 15:27:30 by kosnakam         ###   ########.fr       */
+/*   Updated: 2024/07/20 16:48:51 by kosnakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	sigint_handler_exit(int sig)
+{
+	(void)sig;
+	signal(SIGINT, SIG_DFL);
+	ft_putendl_fd("", 1);
+	rl_on_new_line();
+	kill(0, SIGINT);
+}
+
+void	sig_heredoc_mode(void)
+{
+	t_sig	sa;
+
+	sa.sa_handler = &sigint_handler_exit;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+void	sig_stop(void)
+{
+	struct sigaction	stop_sa;
+
+	stop_sa.sa_handler = SIG_IGN;
+	stop_sa.sa_flags = 0;
+	sigaction(SIGINT, &stop_sa, NULL);
+}
+
 void	read_heredoc(char *eof, int tmp_fd)
 {
 	char	*line;
+	pid_t	pid;
 
-	while (1)
+	sig_stop();
+	pid = fork();
+	if (pid == -1)
+		ft_error("minishell", "fork", "fork failed", 0);
+	if (pid == 0)
 	{
-		line = readline("> ");
-		if (!line || *line == '\n' || (ft_strlen(line) == ft_strlen(eof)
-				&& !ft_strncmp(line, eof, ft_strlen(line))))
+		sig_heredoc_mode();
+		while (1)
 		{
-			if (!line)
+			line = readline("> ");
+			if (!line || *line == '\n' || (ft_strlen(line) == ft_strlen(eof)
+					&& !ft_strncmp(line, eof, ft_strlen(line))))
 			{
-				ft_error("minishell", "warning",
-					"here-document delimited by end-of-file", 0);
-				break ;
-			}
-			if (line[0] == '\n')
-			{
+				if (!line)
+				{
+					ft_error("minishell", "warning",
+						"here-document delimited by end-of-file", 0);
+					exit(0);
+				}
+				if (line[0] == '\n')
+				{
+					free(line);
+					continue ;
+				}
 				free(line);
-				continue ;
+				exit(0);
 			}
+			ft_putendl_fd(line, tmp_fd);
 			free(line);
-			break ;
 		}
-		ft_putendl_fd(line, tmp_fd);
-		free(line);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+		register_signal();
 	}
 }
 
